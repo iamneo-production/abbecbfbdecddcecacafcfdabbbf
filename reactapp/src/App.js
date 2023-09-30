98e2b4fa77bb5644d7f12f3e3765d8113700222e
Iamneo <techadmin@iamneo.ai>
2023-09-30 07:51:14 +0000

    Backup Commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Calculator() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [result, setResult] = useState('');
+  const [error, setError] = useState('');
+
+  const handleAdd = () => {
+    if (!num1 || !num2) {
+      setError('Error: Please enter both numbers');
+      setResult('');
+      return;
+    }
+
+    const sum = parseFloat(num1) + parseFloat(num2);
+    setResult(`Result: ${sum}`);
+    setError('');
+  };
+
+  const handleSubtract = () => {
+    if (!num1 || !num2) {
+      setError('Error: Please enter both numbers');
+      setResult('');
+      return;
+    }
+
+    const difference = parseFloat(num1) - parseFloat(num2);
+    setResult(`Result: ${difference}`);
+    setError('');
+  };
+
+  const handleMultiply = () => {
+    if (!num1 || !num2) {
+      setError('Error: Please enter both numbers');
+      setResult('');
+      return;
+    }
+
+    const product = parseFloat(num1) * parseFloat(num2);
+    setResult(`Result: ${product}`);
+    setError('');
+  };
+
+  const handleDivide = () => {
+    if (!num1 || !num2) {
+      setError('Error: Please enter both numbers');
+      setResult('');
+      return;
+    }
+
+    if (parseFloat(num2) === 0) {
+      setError('Error: Division by zero');
+      setResult('');
+      return;
+    }
+
+    const quotient = parseFloat(num1) / parseFloat(num2);
+    setResult(`Result: ${quotient}`);
+    setError('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Calculator">
+      <input
+        data-testid="num1"
+        type="number"
+        placeholder="Enter first number"
+        value={num1}
+        onChange={(e) => setNum1(e.target.value)}
+      />
+      <input
+        data-testid="num2"
+        type="number"
+        placeholder="Enter second number"
+        value={num2}
+        onChange={(e) => setNum2(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add</button>
+      <button onClick={handleSubtract}>Subtract</button>
+      <button onClick={handleMultiply}>Multiply</button>
+      <button onClick={handleDivide}>Divide</button>
+      {error && <div data-testid="result">{error}</div>}
+      {result && <div data-testid="result">{result}</div>}
     </div>
   );
 }
 
-export default App;
+export default Calculator;
